c8ebd17f60096700119028c9edf180a49c3d18da
taeanasalinas <157439975+taeanasalinas@users.noreply.github.com>
2024-09-16 01:15:19 +0000

    ADDING MAIN

--- MovieManager/Member.h
+++ MovieManager/Member.h
@@ -1,10 +1,21 @@
+#ifndef MEMBER_H
+#define MEMBER_H
 #include "Book.h"
 #include "Library.h"
-
-
+#include<string>
+#include<vector>
 
 using namespace std;
 
-int main(){
-    
+class Member {
+        private:
+        string name;
+        vector<Book> MemberName;
+
+        public:
+        Member();
+        Member(string name);
+
+        string getName() const;
 }
+#endif 
